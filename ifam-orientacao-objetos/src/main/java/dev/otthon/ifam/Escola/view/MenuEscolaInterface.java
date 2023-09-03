9f7e8ee53c9331dc347c91d9f7cf310f5c3f6fb2
Otthon Leão <54039360+otthonleao@users.noreply.github.com>
2023-09-03 02:24:07 -0400

    feat: melhorando o menu

--- ifam-orientacao-objetos/src/main/java/dev/otthon/ifam/Escola/view/MenuEscolaInterface.java
+++ ifam-orientacao-objetos/src/main/java/dev/otthon/ifam/Escola/view/MenuEscolaInterface.java
@@ -20,47 +20,60 @@ public class MenuEscolaInterface {
 		DepartamentoController departamentoController = new DepartamentoController();
 		CampusController campusController = new CampusController();
 		
-		do{
-			System.out.println("1. Cadastrar um Aluno");
-			System.out.println("2. Listar Aluno");
-			System.out.println("3. Cadastrar um Curso");
-			System.out.println("4. Listar Curso");
-			System.out.println("5. Cadastrar Departamento");
-			System.out.println("6. Listar Departamento");
-			System.out.println("7. Cadastrar Campus");
-			System.out.println("8. Listar Campus");
-			System.out.println("9. Sair");
+		do {
+			
+			System.out.println("\n================== MENU ====================");
+			
+			System.out.println("[ 1 ] Cadastrar um Aluno");
+			System.out.println("[ 2 ] Listar Aluno");
+			System.out.println("[ 3 ] Cadastrar um Curso");
+			System.out.println("[ 4 ] Listar Curso");
+			System.out.println("[ 5 ] Cadastrar Departamento");
+			System.out.println("[ 6 ] Listar Departamento");
+			System.out.println("[ 7 ] Cadastrar Campus");
+			System.out.println("[ 8 ] Listar Campus");
+			System.out.println("[ 9 ] Sair");
+			
+			System.out.println("===========================================");
 			System.out.println("Escolha uma Opção: ");
 	
 			opcao = opcaoMenu.nextInt();
 			
 			switch (opcao) {
 			case 1: 
+				System.out.println("\n+++++++++++   CADASTRAR ALUNO   +++++++++++");
 				alunoController.cadastrarAluno();
 				break;
 			case 2:
+				System.out.println("\n*****   LISTA DE ALUNOS CADASTRADOS   *****");
 				alunoController.visualizarAluno();
 				break;
 			case 3:
+				System.out.println("\n+++++++++++   CADASTRAR CURSO   +++++++++++");
 				cursoController.cadastrarCurso();
 				break;
 			case 4:
+				System.out.println("\n*****   LISTA DE CURSOS CADASTRADOS   *****");
 				cursoController.visualizarCurso();
 				break;
 			case 5:
+				System.out.println("\n++++++++   CADASTRAR DEPARTAMENTO   ++++++++");
 				departamentoController.cadastrarDepartamento();
 				break;
 			case 6:
+				System.out.println("\n*****   DEPARTAMENTOS CADASTRADOS   *****");
 				departamentoController.visualizarDepartamento();
 			    break;    
 			case 7:
+				System.out.println("\n+++++++++++   CADASTRAR CAMPUS   +++++++++++");
 				campusController.cadastrarCampus();
 			    break;
 			case 8:
+				System.out.println("\n*****   CAMPUS CADASTRADOS   *****");
 				campusController.visualizarCampus();
 			    break;
 			case 9:
-				System.out.printf("\nFECHANDO PROGRAMA\n");
+				System.out.printf("\n=-=-=-=-=-=-=-=  FECHANDO PROGRAMA  =-=-=-=-=-=-=-=\n");
 				break;
             default:
             	System.out.printf("\nOpção inválida, Tente novamente\n");
